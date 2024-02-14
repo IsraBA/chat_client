de40a965575fb1c04421d9c96d889ae0a4c20c15
eliraz <elish7@gmail.com>
2024-02-14 20:12:59 +0200

    eliraz

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-export function Events({ events }) {
+export function Events({ events, userName="annonymous", color="black" }) {
+
+
+
     return (
         <ul className="main-chat">
-            {
-                events.map((data, index) =>
-                    <li key={index}>
-                        <div>{data.username}: {data.message}</div>
-                        <div>{data.time}</div>
-                    </li>
-                )
-            }
+            {events.map((data, index) => (
+                <li key={index}>
+                    <div className={data.username === userName ? 'myName' : ''}>
+                    <b style={{color: color}}>{data.username}:</b> {data.message}
+                    </div>
+                    <div>{data.time}</div>
+                </li>
+            ))}
         </ul>
     );
 }
